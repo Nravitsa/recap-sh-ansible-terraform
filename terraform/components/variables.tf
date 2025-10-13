ad152cc3f8776c38bf8821cce4696896fb4252a2
tushar <tusharlevesl@icloud.com>
2025-10-12 21:52:17 -0400

    .

---
+++ terraform/components/variables.tf
@@ -0,0 +1,16 @@
+
+
+variable "name" {}
+variable "resource_group_name" {}
+variable "zone_name" {}
+
+
+
+variable "subnet_id" {}
+variable "name" {}
+variable "name" {}
+variable "name" {}
+variable "name" {}
+variable "name" {}
+
+
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+
+#WITH VALUIESSS BECAUE WE ARE RUNING MAILN CODE OUTSIFE THE FUNCITION/MODULES
+variable "name" {}
+variable "resource_group_name" {}
+variable "zone_name" {}
+
+
+
+variable "subnet_id" {}
+variable "name" {}
+variable "name" {}
+variable "name" {}
+variable "name" {}
+variable "name" {}
+
+
