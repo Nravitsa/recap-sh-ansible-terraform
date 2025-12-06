8407123f4869cfc92cb88ea849f5db3fef891707
tushar <tusharlevesl@icloud.com>
2025-12-06 16:40:39 -0500

    .

--- terrafrom2/variables/main.tf
+++ terrafrom2/variables/main.tf
@@ -13,3 +13,57 @@ variable "name" {
 output "name" {
   value = var.name
 }
+
+
+
+variable "CurrentUSDtoINT" {
+  default = 90
+}
+
+output "outputUSDtoINR" {
+  value = "${var.CurrentUSDtoINT} is the current USD to INR price"
+}
+
+
+
+
+variable "list" {
+  default = [
+    1,
+    2,
+    false
+    true
+    "Sexy Boiii"
+  ]
+}
+
+
+output "listOutput" {
+  value = [
+    var.list[0]
+    var.list[1]
+    var.list[2]
+    var.list[3]
+    var.list[4]
+  ]
+}
+
+
+variable "map" {
+  default = {
+    name: "Tush"
+    age: 70
+    HasKids: false
+    }
+}
+
+
+output "mapOutput" {
+  value = [
+    var.map["name"]
+    var.map["age"]
+    var.map["HasKids"]
+  ]
+}
+
+# git pull terraform init terraform plan
