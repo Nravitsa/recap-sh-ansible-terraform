597bc8390fc935dadb72987a9afdc783f207c06e
tushar <tusharlevesl@icloud.com>
2025-12-06 16:18:53 -0500

    .

---
+++ terrafrom2/variables/main.tf
@@ -0,0 +1,19 @@
+variable "age" {
+  default = 30
+}
+
+output "output_of_age" {
+  value = var.age
+}
+
+#data types
+#Str num boolean
+
+
+variable "name" {
+  default = "KamKAMAS"
+}
+
+output "output_name" {
+  value = var.name
+}
